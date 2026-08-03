df59acaa5b7c7d2605df3595b08996ddb5b8b186
Michael Dalesio <michael.dalesio@gmail.com>
2026-08-03 12:49:25 -0400

    Adds EtherIP support

--- iocBoot/iocupwtApp/st.cmd
+++ iocBoot/iocupwtApp/st.cmd
@@ -11,7 +11,22 @@ cd "${TOP}"
 dbLoadDatabase "dbd/upwt.dbd"
 upwt_registerRecordDeviceDriver pdbbase
 
+## Initialize EtherIP driver infrastructure
+# Syntax: drvEtherIP_init(stack_size, queue_size, direct_cup)
+# - direct_cpu = 0 (standard/safe EPICS thread handling)
+drvEtherIP_init(0, 0, 0)
+
+## Define PLC connections
+# Syntax: drvEtherIP_define_PLC("PLC_NAME", "IP_ADDRESS_OR_HOSTNAME", SLOT_NUMBER)
+drvEtherIP_define_PLC("PLC_1", "192,168.1.10", 0)
+
+## (Optional) Set custom scan rate update intervals for tags
+# Syntax: EIP_set_scan_period(period_index, seconds)
+# EIP_set_scan_period(1, 0.1) # Period 1 = 100ms
+# EIP_set_scan_period(2, 0.5) # Period 2 = 500ms
+
 ## Load record instances
+# dbLoadRecords("db/<plc.db>", "PLC=<PLC_NAME>")
 dbLoadRecords("db/flo_aiao.db")
 dbLoadRecords("db/flo_DAS1a.db")
 dbLoadRecords("db/flo_DAS1b.db")
